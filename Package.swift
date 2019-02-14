087ea80d67ed8a0b7e7078dd085ba5f67c74a56c
Christopher Prince <chris@SpasticMuffin.biz>
2019-02-14 05:36:33 -0700

    version 0.2.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,8 @@ let package = Package(
         .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMajor(from: "2.4.0")),
         
         // .package(url: "https://github.com/benspratling4/SwiftAWSSignatureV4.git", from:"1.0.0")
-        .package(url: "https://github.com/crspybits/SwiftAWSSignatureV4.git", .branch("master"))
+        // .package(url: "https://github.com/crspybits/SwiftAWSSignatureV4.git", .branch("master"))
+        .package(url: "https://github.com/crspybits/SwiftAWSSignatureV4.git", from:"1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
